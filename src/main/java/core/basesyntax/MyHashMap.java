5c84b8dba5ad7a068e90d45cbeeb28a6d078ba67
Maksym <pozetiv13@gmail.com>
2026-05-24 18:31:59 +0300

    Maksym_Gorodnychyi

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,88 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        int index = getIndex(key);
+        Node<K, V> firstNode = table[index];
+        while (firstNode != null) {
+            if (keysEqual(key, firstNode.key)) {
+                firstNode.value = value;
+                return;
+            }
+            firstNode = firstNode.next;
+        }
+        if (size + 1 > table.length * LOAD_FACTOR) {
+            resize();
+            index = getIndex(key);
+        }
+        table[index] = new Node<>(key, value, table[index]);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K, V> current = table[index];
+
+        while (current != null) {
+            if (keysEqual(key, current.key)) {
+                return current.value;
+            } else {
+                current = current.next;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getIndex(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return Math.abs(key.hashCode() % table.length);
+    }
+
+    private boolean keysEqual(K firstKey, K secondKey) {
+        return firstKey == null ? secondKey == null : firstKey.equals(secondKey);
+    }
+
+    private void resize() {
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * 2];
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                Node<K, V> next = node.next;
+                put(node.key, node.value);
+                node = next;
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
